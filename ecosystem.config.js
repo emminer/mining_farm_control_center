f28ed2f80460fc08a3d1a05d945399aeb4a5f42e
yuanbo <yuanbo@kuaibangxing.com>
2017-09-21 08:50:57 +0800

    add pm2 config file pm2 start ecosystem.config.js --env production

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps : [
+      {
+        name: "mfcc",
+        script: "./bin/www",
+        watch: true,
+        env: {
+            "PORT": 3000,
+            "NODE_ENV": "development"
+        },
+        env_production: {
+            "PORT": 3001
+            "NODE_ENV": "production",
+        }
+      }
+  ]
+}
