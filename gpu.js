9bf030640a52328afbb8a364d184073bbf0e13c8
yuanbo <yuanbo@kuaibangxing.com>
2017-09-23 02:10:43 +0800

    Get gnu via ssh

---
+++ gpu.js
@@ -0,0 +1,20 @@
+const ssh = require('./ssh');
+
+const query = 'index,temperature.gpu,fan.speed,utilization.gpu';
+
+function parseLine(data) {
+  const arr = data.split(',');
+  return {
+    index: +(arr[0]),
+    temp: +(arr[1]),
+    fan: +(arr[2]),
+    utilization: +(arr[3]),
+  };
+}
+
+module.exports = function(rigIp) {
+  return ssh(rigIp, 'm1', `nvidia-smi --format=csv,noheader,nounits --query-gpu=${query}`)
+  .then(stdout => {
+    return stdout.split('\n').filter(l => (l.indexOf(',') > 0)).map(parseLine);
+  });
+};
