47fc3132be0f7a3e3888f6f1131fbd26c10b6a7f
yuanbo <yuanbo@kuaibangxing.com>
2017-10-01 18:53:19 +0800

    Config ping

--- ping.js
+++ ping.js
@@ -3,6 +3,7 @@ var Promise = require('bluebird');
 
 module.exports = function(targets) {
   return Promise.map(targets, target => {
-    return ping.promise.probe(target).then(res => res.alive).catch(() => false);
+    return ping.promise.probe(target, {timeout: 10, min_reply: 4})
+    .then(res => res.alive).catch(() => false);
   });
 };
