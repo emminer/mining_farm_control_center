c025d57f1d4e1e5baf189b98a6d0b4335e014dff
yuanbo <yuanbo@kuaibangxing.com>
2017-10-01 21:06:05 +0800

    add watts to calc profit

--- config.template.js
+++ config.template.js
@@ -16,10 +16,10 @@ const api = {
 }
 
 const rigs = [
-  {name: 'nv100', ip: '192.168.1.100', pin: '8', coin: 'ETH', pool: 'ethermine', min_hashrate: 1800},
-  {name: 'nv101', ip: '127.0.0.1', pin: '10', coin: 'ETH', pool: 'ethermine', min_hashrate: 180},
-  //{name: 'nv167', ip: '192.168.1.167', pin: '12', coin: 'ETH', pool: 'ethermine', min_hashrate: 190},
-  {name: 'nv129', ip: '10.0.0.129', pin: '3', coin: 'ZEC', pool: 'flypool_zcash', min_hashrate: 1.5},
+  {name: 'nv100', ip: '192.168.1.100', pin: '8', coin: 'ETH', pool: 'ethermine', min_hashrate: 1800, watts: 900},
+  {name: 'nv101', ip: '127.0.0.1', pin: '10', coin: 'ETH', pool: 'ethermine', min_hashrate: 180, watts: 1000},
+  //{name: 'nv167', ip: '192.168.1.167', pin: '12', coin: 'ETH', pool: 'ethermine', min_hashrate: 190, watts: 1200},
+  {name: 'nv129', ip: '10.0.0.129', pin: '3', coin: 'ZEC', pool: 'flypool_zcash', min_hashrate: 1.5, watts: 900},
 ];
 
 const pools = [
